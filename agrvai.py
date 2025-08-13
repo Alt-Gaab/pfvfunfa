5090a8da35608e4dd8385c50cafd4a60ef233c77
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-13 12:43:08 -0300

    loco

--- agrvai.py
+++ agrvai.py
@@ -1,6 +1,6 @@
 x =int(input("Digite um valor de x:"))
 y =int(input("Digite um valor de y:"))
-somar = x + y
-subitrair = x - y
+somar = x + 5
+subitrair = y - 3
 print("o valor é", somar)
 print("o valor é", subitrair)
