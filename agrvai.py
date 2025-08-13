c46a76419b3d7aebf54b5f0c3cffc841be455149
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-13 12:38:28 -0300

    sera

--- agrvai.py
+++ agrvai.py
@@ -1,1 +1,6 @@
-print("porfavor funciona.")
+x =int(input("Digite um valor de x:"))
+y =int(input("Digite um valor de y:"))
+somar = x + y
+subitrair = x - y
+print("o valor é", somar)
+print("o valor é", subitrair)
