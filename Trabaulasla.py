fec10f912fc53f456cbed3b955fb7747d4f0c247
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-18 10:31:10 -0300

    ...

---
+++ Trabaulasla.py
@@ -0,0 +1,8 @@
+print("Digiti sua idade:")
+IDADE = int(input())
+if IDADE < 18:
+    print("Sai fora moleque.")
+    print("Porra, vai comer terra.")
+
+if IDADE >= 18:
+    print("Você esta ficando MUITO velho.")
