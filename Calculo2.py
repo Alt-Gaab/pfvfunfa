ed4a5246a3a9b2a157a1e089696fa35e29c7ca9c
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-18 09:46:20 -0300

    Aulinha

---
+++ Calculo2.py
@@ -0,0 +1,4 @@
+valor = int(input("Digite um valor do produto: "))
+quantidade = int(input("Digite a quantidade de parcelas: "))
+parcelas = valor / quantidade
+print("O valor da parcela será R$", parcelas)
---
+++ Calculo3.py
@@ -0,0 +1,5 @@
+numero = int(input(" Digite um numero: "))
+quadrado = numero ** 2
+cubo = numero ** 3
+print("O seu quadrado é:", quadrado)
+print("O seu cubo é:", cubo)
