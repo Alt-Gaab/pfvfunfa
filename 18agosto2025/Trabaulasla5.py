7a5f9eaf87f6ca438b13bf2b9cecfae2202c4032
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-18 12:45:15 -0300

    .

---
+++ 18agosto2025/Trabaulasla5.py
@@ -0,0 +1,25 @@
+print("Escolha um número de acordo com o dia da semana em que você esta para que você possa ver o que pedo fazer hoje:")
+print("1 domingo")
+print("2 segunda")
+print("3 terça")
+print("4 quarta")
+print("5 quinta")
+print("6 sexta")
+print("7 sabado")
+DIA = input()
+
+match DIA:
+    case 1:
+        print("Sla porra, faz o q vc quizer.")
+    case 2:
+        print("E eu lá sou dono da tua vida?")
+    case 3:
+        print("Faz o q vc quizer porra, pra mim tanto faz.")
+    case 4:
+        print("Vai viver a tua vida caralho.")
+    case 5:
+        print("Sla, taca o foda-se e faz o q quizer.")
+    case 6:
+        print("Faz qualquer merda msm.")
+    case 7:
+        print("Ta sem nd pra fzr? Vai dar o cu então.")
