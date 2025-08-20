c5dea63f794005d6c0aed2bcd4c0faafe2284d5c
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-20 12:39:01 -0300

    .

---
+++ 18agosto2025/Trabaulasla2.2.py
@@ -0,0 +1,15 @@
+print("Digite suas quatro notas:")
+N1 = int(input())
+N2 = int(input())
+N3 = int(input())
+N4 = int(input())
+média = N1 + N2 + N3 + N4
+somar = média / 4
+print(somar)
+if somar <= 5:
+    print("Burro do caralho! Vai fazer o ano denovo!")
+if somar >= 6.5:
+    print("Parabéns, você não é um burro do caralho.")
+else:
+    somar < 5 or somar > 6.5
+    print("Você ainda tem esperança de não ser um burro do caralho.")
