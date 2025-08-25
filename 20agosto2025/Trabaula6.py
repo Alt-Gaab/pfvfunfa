2661963a8be50c809f3eb1fe2ec77f49fc525d8b
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-25 09:50:49 -0300

    Tem alguma coisa errada

--- 20agosto2025/Trabaula6.py
+++ 20agosto2025/Trabaula6.py
@@ -1,15 +1,18 @@
+#Tem alguma coisa errada
 print("Cada um dos dois jogaores de escolher entre pedra, papel, e tesoura:")
-J1 = input("Jogador 1, escolha:")
-J2 = input("Jogador 2, escolha:")
+J1 = str(input("Jogador 1, escolha:"))
+J2 = str(input("Jogador 2, escolha:"))
+if J1 and J2 != "pedra" or "tesoura" or "papel":
+    print("Isso é pedra, papel e tesoura imbecil.")
 
 "tesoura" > "papel"
 "papel" > "pedra"
 "pedra" > "tesoura"
 
-if J1 > J2:
+if J1 < J2:
     print("Jogador 1 vence!")
 
-if J2 > J1:
+if J2 < J1:
     print("Jogador 2 vence!")
 
 if J1 == J2:
