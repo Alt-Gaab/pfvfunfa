61a412c173e38bcf050853a2d724450bbacb91bd
Gaab-alt <gabrielscavalcanti16@gmail.com>
2025-08-25 09:27:47 -0300

    .

--- 20agosto2025/Trabaula6.py
+++ 20agosto2025/Trabaula6.py
@@ -1,22 +1,16 @@
-#Achar um jeito mais simples
 print("Cada um dos dois jogaores de escolher entre pedra, papel, e tesoura:")
 J1 = input("Jogador 1, escolha:")
 J2 = input("Jogador 2, escolha:")
-if J1 == "tesoura" and J2 == "pedra":
-    print("Jogador 2 vence")
-if J1 == "tesoura" and J2 == "papel":
-    print("Jogador 1 vence")
-if J1 == "tesoura" and J2 == "tesoura":
-    print("Empate")
-if J1 == "papel" and J2 == "tesoura":
-    print("Jogador 2 vence")
-if J1 == "papel" and J2 == "pedra":
-    print("Jogador 1 vence")
-if J1 == "papel" and J2 == "papel":
-    print("Empate")
-if J1 == "pedra" and J2 == "papel":
-    print("Jogador 2 vence")
-if J1 == "pedra" and J2 == "tesoura":
-    print("Jogador 1 vence")
-if J1 == "pedra" and J2 == "pedra":
-    print("Empate")
+
+"tesoura" > "papel"
+"papel" > "pedra"
+"pedra" > "tesoura"
+
+if J1 > J2:
+    print("Jogador 1 vence!")
+
+if J2 > J1:
+    print("Jogador 2 vence!")
+
+if J1 == J2:
+    print("Empate!")
